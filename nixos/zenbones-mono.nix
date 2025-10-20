4ad8d9f8ad0e3a1a3d711f5ba0d594b838096c47
0gnjen01 <ognjenk0l3@gmail.com>
2025-10-20 17:54:57 +0200

    font

--- nixos/zenbones-mono.nix
+++ nixos/zenbones-mono.nix
@@ -1,25 +1,22 @@
 {
   lib,
   stdenv,
-  fetchFromGitHub,
+  fetchzip,
 }:
 
 stdenv.mkDerivation rec {
   pname = "zenbones-mono";
   version = "2.400";
 
-  src = fetchFromGitHub {
-    owner = "mcchrish";
-    repo = "${pname}";
-    rev = "v${version}";
-    sha256 = "104kid04mc526g9xaa9716b2bsrjmx7jgxi2h5731h6060b8ln4c";
+  src = fetchzip {
+    url = "https://github.com/zenbones-theme/zenbones-mono/releases/download/v${version}/Zenbones-Mono-TTF.zip";
+    sha256 = "0avwa96ipwcayi5mqh1hfbq6nj1i2hqn05v1x5h217953vkczx0p";
   };
 
   installPhase = ''
     runHook preInstall
 
     install -Dm644 *.ttf -t $out/share/fonts/truetype
-    install -Dm644 *.woff2 -t $out/share/fonts/woff2
 
     runHook postInstall
   '';
