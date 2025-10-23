9e3251e113526584d564e06430cc9aee1db2dbbb
0gnjen01 <ognjenk0l3@gmail.com>
2025-10-23 17:52:26 +0200

    updating in case windwos eats my nixos

--- modules/home/river/i3bar.nix
+++ modules/home/river/i3bar.nix
@@ -10,7 +10,7 @@
       margin_bottom = 0;
       margin_left = 0;
       margin_top = 0;
-      separator = "#B4BDC3FF"";
+      separator = "#B4BDC3FF";
       "wm.river" = {
         max_tag = 0;
       };
